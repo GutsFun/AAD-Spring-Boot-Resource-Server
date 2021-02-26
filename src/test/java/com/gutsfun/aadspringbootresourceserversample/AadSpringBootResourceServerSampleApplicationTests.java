5d2146701269de7e5d06c5a03917869d80d8e1ed
Cyrille Perrot <cperrot@ingeniance.fr>
2021-02-26 13:01:09 +0100

    init project

---
+++ src/test/java/com/gutsfun/aadspringbootresourceserversample/AadSpringBootResourceServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutsfun.aadspringbootresourceserversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadSpringBootResourceServerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
